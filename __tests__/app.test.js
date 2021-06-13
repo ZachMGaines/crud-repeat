5c76cabca56367424e28a70fb6c5d68117516961
Zachary Gaines <zacharygaines28@gmail.com>
2021-06-12 19:28:22 -0700

    working on post

--- __tests__/app.test.js
+++ __tests__/app.test.js
@@ -7,4 +7,22 @@ describe('demo routes', () => {
   beforeEach(() => {
     return setup(pool);
   });
+
+  it('creates a beer via POST', async () => {
+    const res = request(app)
+      .post('/api/v1/beers')
+      .send({
+        name: 'barney',
+        abv: '9%',
+        color: 'dark'
+      });
+    expect(res.body).toEqual({
+      id: '1',
+      name: 'barney',
+      abv: '9%',
+      color: 'dark'
+    });
+  });
+
 });
+
---
+++ sql/setup.sql
@@ -0,0 +1,8 @@
+DROP TABLE IF EXISTS beers;
+
+CREATE TABLE beers (
+  id INTEGER NOT NULL,
+  name TEXT NOT NULL,
+  age INTEGER NOT NULL,
+  color TEXT
+);
