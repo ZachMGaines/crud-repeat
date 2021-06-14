15b281afd019b95b325d35bef2f71af6b9539539
Zachary Gaines <zacharygaines28@gmail.com>
2021-06-14 11:02:42 -0700

    2nd test failing

--- __tests__/app.test.js
+++ __tests__/app.test.js
@@ -2,6 +2,7 @@ import pool from '../lib/utils/pool.js';
 import setup from '../data/setup.js';
 import request from 'supertest';
 import app from '../lib/app.js';
+import Beer from '../lib/models/Beer.js';
 
 describe('demo routes', () => {
   beforeEach(() => {
@@ -13,6 +14,16 @@ describe('demo routes', () => {
       .post('/api/v1/beers')
       .send({ name: 'barney', abv: '9%', color: 'dark' });
     expect(res.body).toEqual({ id: '1', name: 'barney', abv: '9%', color: 'dark' });
+  });
+
+  it('finds a beer via GET', async () => {
+    const beer = await Beer.insert({
+      name: 'lagunitas',
+      abv: '6.8%',
+      color: 'medium dark'
+    });
+    const res = await request(app).get(`/api/v1/beers/${beer.id}`);
+    expect(res.body).toEqual(beer);
   });
 
 });
