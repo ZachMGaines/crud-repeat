e0641975c2cc66faa874dbee41c4b93ef7f6a663
Zachary Gaines <zacharygaines28@gmail.com>
2021-06-15 09:06:02 -0700

    new setup file

--- data/setup.js
+++ data/setup.js
@@ -1,20 +1,12 @@
-import 'dotenv/config.js';
 import { promises as fs } from 'fs';
 import path from 'path';
-
 export default (pool) => {
-
-  const pathToSlice = `${path.dirname(new URL(import.meta.url).pathname)}/../sql/setup.sql`;
-  const replacedPath = pathToSlice.slice(1, pathToSlice.length);
-  const filePath = replacedPath.replace('%', ' ');
-
   return fs
     .readFile(
-      filePath,
+      './sql/setup.sql',
       {
         encoding: 'utf-8',
       }
     )
     .then((sql) => pool.query(sql));
 };
-
